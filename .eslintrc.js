46fd2a4f04224e07ca7cc5f4821c4cd7c2d7b642
Céline Louvet <clouvet@hubside.com>
2021-05-18 15:52:36 +0200

    ✨ Add nuxt skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:prettier/recommended",
+    "plugin:nuxt/recommended",
+  ],
+  plugins: [],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["double"],
+  },
+};
