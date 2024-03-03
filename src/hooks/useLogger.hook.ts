1ad60eddb71f724f33bd1bb22c34f5d3be05df82
Céline Louvet <celine.louvet@shine.fr>
2024-03-03 20:41:47 +0100

    Add event for color mode change

--- src/hooks/useLogger.hook.ts
+++ src/hooks/useLogger.hook.ts
@@ -1,15 +1,18 @@
+import { useColorMode } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 export const useLogger = () => {
+  const { colorMode } = useColorMode();
   const { i18n } = useTranslation();
+
   const language = i18n.language;
 
   return {
-    log: (
+    log: async (
       message: string,
       metadata: Record<string, string | boolean | null | undefined> = {}
     ) => {
-      const body = { message, metadata: { ...metadata, language } };
+      const body = { message, metadata: { ...metadata, colorMode, language } };
 
       return fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/logger`, {
         method: 'POST',
