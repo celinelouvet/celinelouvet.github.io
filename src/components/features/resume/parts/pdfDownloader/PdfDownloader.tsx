fd9a99ffd77a284c836a49644eadd4af2eddd344
Céline Louvet <celine.louvet@shine.fr>
2024-03-03 20:33:56 +0100

    Add event for PDF download

--- src/components/features/resume/parts/pdfDownloader/PdfDownloader.tsx
+++ src/components/features/resume/parts/pdfDownloader/PdfDownloader.tsx
@@ -3,10 +3,13 @@ import { type FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import { BsFileEarmarkPdfFill } from 'react-icons/bs';
 
+import { useLogger } from '@/hooks';
+
 const root =
   'https://storage.googleapis.com/celinelouvet-cv.appspot.com/resume';
 
 export const PdfDownloader: FC = () => {
+  const { log } = useLogger();
   const { i18n } = useTranslation();
 
   const file = `${root}/LOUVET_Celine_${i18n.language.toUpperCase()}.pdf`;
@@ -14,7 +17,11 @@ export const PdfDownloader: FC = () => {
   return (
     <Link href={file} download={fileName} isExternal>
       <Tooltip label="Download PDF">
-        <Button leftIcon={<BsFileEarmarkPdfFill />} size="sm">
+        <Button
+          leftIcon={<BsFileEarmarkPdfFill />}
+          size="sm"
+          onClick={() => log('PDF')}
+        >
           PDF
         </Button>
       </Tooltip>
