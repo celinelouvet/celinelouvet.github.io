ef16b9571a744cfc89d32d3a0f340d4af0f6aef0
Céline Louvet <celine.louvet@shine.fr>
2024-03-09 18:13:02 +0100

    Fix color mode for logger

--- src/hooks/useLogger.hook.ts
+++ src/hooks/useLogger.hook.ts
@@ -1,18 +1,30 @@
-import { useColorMode } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 
 export const useLogger = () => {
-  const { colorMode } = useColorMode();
   const { i18n } = useTranslation();
 
   const language = i18n.language;
+
+  const getColorMode = () => {
+    const theme = document
+      .getElementsByTagName('body')[0]!
+      .getAttribute('class');
+    return theme?.includes('dark') ? 'dark' : 'light';
+  };
 
   return {
     log: async (
       message: string,
       metadata: Record<string, string | boolean | null | undefined> = {}
     ) => {
+      const colorMode = getColorMode();
+
       const body = { message, metadata: { ...metadata, colorMode, language } };
+
+      if (process.env.NODE_ENV === 'development') {
+        console.log('Logging: ', body);
+        return;
+      }
 
       return fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/logger`, {
         method: 'POST',
