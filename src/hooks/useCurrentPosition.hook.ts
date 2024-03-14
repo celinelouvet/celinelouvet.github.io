1046d4354bfb29414e983c679c2ffa3e6bb2f8b2
Céline Louvet <celine.louvet@shine.fr>
2024-03-14 15:46:19 +0100

    Handle swipe gestures

--- src/hooks/useCurrentPosition.hook.ts
+++ src/hooks/useCurrentPosition.hook.ts
@@ -12,6 +12,9 @@ export const useCurrentPosition = (length: number) => {
   useEffect(() => {
     const maxPosition = length - 1;
 
+    let startPosition: number | null = null;
+    let endPosition: number | null = null;
+
     const channel = new BroadcastChannel('useCurrentPosition');
 
     const next = () => {
@@ -30,6 +33,35 @@ export const useCurrentPosition = (length: number) => {
       setCurrentPosition(newPosition);
     };
 
+    const onTouchStart = ({ targetTouches }: TouchEvent) => {
+      const [touch] = targetTouches;
+      if (touch && touch.clientX) {
+        startPosition = touch.clientX;
+      }
+    };
+
+    const onTouchMove = ({ targetTouches }: TouchEvent) => {
+      const [touch] = targetTouches;
+      if (touch && touch.clientX) {
+        endPosition = touch.clientX;
+      }
+    };
+
+    const onTouchEnd = () => {
+      if (startPosition && endPosition) {
+        if (startPosition > endPosition) {
+          next();
+        } else {
+          back();
+        }
+      }
+      if (startPosition && !endPosition) {
+        next();
+      }
+      startPosition = null;
+      endPosition = null;
+    };
+
     const onKeyDown = (event: KeyboardEvent) => {
       if (nextKeys.includes(event.key)) {
         next();
@@ -46,10 +78,16 @@ export const useCurrentPosition = (length: number) => {
 
     channel.addEventListener('message', onMessage);
     document.addEventListener('keydown', onKeyDown);
+    document.addEventListener('touchstart', onTouchStart);
+    document.addEventListener('touchmove', onTouchMove);
+    document.addEventListener('touchend', onTouchEnd);
 
     return () => {
       channel.removeEventListener('message', onMessage);
       document.removeEventListener('keydown', onKeyDown);
+      document.removeEventListener('touchstart', onTouchStart);
+      document.removeEventListener('touchmove', onTouchMove);
+      document.removeEventListener('touchend', onTouchEnd);
     };
   }, [currentPosition, length]);
 
