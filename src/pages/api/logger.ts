fa6a1a9d0556fb8ae6b46715943a3cd2950bf792
Céline Louvet <celine.louvet@shine.fr>
2024-03-31 16:17:05 +0200

    Clean API logger code

--- src/pages/api/logger.ts
+++ src/pages/api/logger.ts
@@ -1,7 +1,6 @@
-import { Datastore } from '@google-cloud/datastore';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { v4 as uuid } from 'uuid';
-import { z } from 'zod';
+
+import { createLog } from '@/api';
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,39 +10,11 @@ export default async function handler(
     return res.status(400).json({ error: 'Bad request' });
   }
 
-  const body = loggerSchema.safeParse(req.body);
-  if (!body.success) {
+  try {
+    await createLog(req);
+
+    return res.status(200).send();
+  } catch (error) {
     return res.status(400).json({ error: 'Bad request' });
   }
-
-  await saveLog(body.data);
-
-  return res.status(200).send();
 }
-
-const loggerSchema = z.object({
-  message: z.string(),
-  metadata: z.record(z.string().or(z.boolean()).nullable().optional()),
-});
-type Logger = z.infer<typeof loggerSchema>;
-
-const saveLog = async ({ message, metadata }: Logger) => {
-  const datastore = new Datastore();
-
-  const kind = 'Tracking';
-  const id = uuid();
-  const key = datastore.key([kind, id]);
-
-  const task = {
-    key,
-    data: {
-      ...metadata,
-      id,
-      message,
-      timestamp: new Date().toISOString(),
-      version: process.env.VERSION_NAME,
-    },
-  };
-
-  await datastore.save(task);
-};
