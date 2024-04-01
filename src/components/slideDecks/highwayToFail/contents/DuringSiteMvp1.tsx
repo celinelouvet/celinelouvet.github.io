b59caa9317ca0aea2675753409a0d3284ac56d06
Céline Louvet <celine.louvet@shine.fr>
2024-04-01 14:53:47 +0200

    Change title for site MVP slides

--- src/components/slideDecks/highwayToFail/contents/DuringSiteMvp1.tsx
+++ src/components/slideDecks/highwayToFail/contents/DuringSiteMvp1.tsx
@@ -7,7 +7,7 @@ import { VerticalTimelineDuring } from '../components';
 export const content = (
   <SlideContent
     type={SlideContentTypes.titleWithThinColumn}
-    title="Premier gouffre de temps"
+    title="Première perte de temps"
     alignContent="flex-start"
     column1={<VerticalTimelineDuring />}
   >
