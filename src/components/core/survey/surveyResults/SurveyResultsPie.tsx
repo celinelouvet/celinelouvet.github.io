ae6d2b5f1e1ca2010c7193d990acc8b0e9a4dffd
Céline Louvet <celine.louvet@shine.fr>
2024-04-28 17:06:36 +0200

    Add empty pie chart

--- src/components/core/survey/surveyResults/SurveyResultsPie.tsx
+++ src/components/core/survey/surveyResults/SurveyResultsPie.tsx
@@ -19,15 +19,36 @@ type SurveyResultsPieProps = Omit<BoxProps, 'results'> & {
 export const SurveyResultsPie = forwardRef<SurveyResultsPieProps, 'div'>(
   ({ question, results, ...props }, ref) => {
     const style = useStyleConfig('SurveyResultsPie');
+    const gray500 = useToken('colors', 'gray.500');
     const brand900 = useToken('colors', 'brand.900');
 
     const { choices } = question;
 
-    const data = choices.map(({ text, value, color }) => ({
-      title: text,
-      value: results[value] ?? 0,
-      color,
-    }));
+    const data = choices.map(({ text, value, color }) => {
+      const newValue = results[value] ?? 0;
+
+      return { title: text, value: newValue, color };
+    });
+
+    const total = Object.values(results).reduce((acc, value) => acc + value, 0);
+    if (total === 0)
+      return (
+        <Center ref={ref} sx={style}>
+          <Box {...props}>
+            <PieChart
+              data={[{ title: 'No votes', value: 1, color: gray500 }]}
+              label={({ dataEntry }) => dataEntry.title}
+              labelStyle={{
+                fontSize: '0.5em',
+                fill: brand900,
+                fontFamily: 'nunito',
+                fontWeight: 'bold',
+              }}
+              radius={42}
+            />
+          </Box>
+        </Center>
+      );
 
     return (
       <Center ref={ref} sx={style}>
