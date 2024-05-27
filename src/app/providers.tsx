d478445b9c8d50493c701437e9f09d456c2e0ed8
Céline Louvet <celine.louvet@shine.fr>
2024-05-27 17:25:40 +0200

    Inject i18n context into app router

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import '@/lib/i18n/config';
 import theme from '@/theme';
 
 export function Providers({ children }: { children: React.ReactNode }) {
