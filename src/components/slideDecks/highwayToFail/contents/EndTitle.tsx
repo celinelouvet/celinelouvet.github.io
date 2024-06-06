8f8256f379957a144215f5824f257f875922851d
Céline Louvet <celine.louvet@shine.fr>
2024-06-06 11:36:04 +0200

    Add feedbacks QR code

--- src/components/slideDecks/highwayToFail/contents/EndTitle.tsx
+++ src/components/slideDecks/highwayToFail/contents/EndTitle.tsx
@@ -1,12 +1,58 @@
-import { Text } from '@chakra-ui/react';
+import { Center, Grid, Stack, Text } from '@chakra-ui/react';
+import { type FC } from 'react';
 
-import { SlideContent, SlideContentTypes, SlideNote } from '@/components/core';
+import {
+  ColorMode,
+  H1Heading,
+  H2Heading,
+  QrCode,
+  SlideContent,
+  SlideContentTypes,
+  SlideNote,
+} from '@/components/core';
 
-export const content = <SlideContent type={SlideContentTypes.endTitle} />;
+const Title: FC<{ title: string }> = ({ title }) =>
+  title.split('\n').map((line, i) => (
+    <H1Heading
+      key={i}
+      variant="slide"
+      size="lg"
+      textAlign="center"
+      lineHeight="1.5"
+    >
+      {line}
+    </H1Heading>
+  ));
+
+export const content = (
+  <SlideContent colormode={ColorMode.dark} type={SlideContentTypes.fullContent}>
+    <Grid
+      templateColumns="3fr 2fr"
+      gap="1em"
+      width="100%"
+      height="100%"
+      justifyContent="stretch"
+    >
+      <Center>
+        <Title title="Merci pour votre attention" />
+      </Center>
+      <Stack textAlign="center">
+        <QrCode value="https://openfeedback.io/z5LkRLNe9J8At7OFYDz6/2024-06-06/highway-to-fail" />
+        <H2Heading variant="slide">Retours</H2Heading>
+      </Stack>
+    </Grid>
+  </SlideContent>
+);
+
+// export const content = <SlideContent type={SlideContentTypes.endTitle} />;
 
 const note = (
   <SlideNote>
     <Text>Voilà, merci pour votre attention.</Text>
+    <Text>
+      Je vous ai mis le QR code pour les feedbacks, si vous avez envie d’en
+      laisser un.
+    </Text>
     <Text>
       Je reste dans le coin pour quelques temps, donc n’hésitez pas à venir
       discuter.
