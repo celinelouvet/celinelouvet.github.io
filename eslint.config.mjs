270b387194b2df4de0faf371eb2c4c9c2c30c160
Céline Louvet <celine.louvet@shine.fr>
2024-11-23 23:17:19 +0100

    add storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 import tseslint from "typescript-eslint";
-import { fixupConfigRules } from "@eslint/compat";
 
 const compat = new FlatCompat();
 
@@ -10,14 +9,18 @@ const importPlugin = compat.extends(
   "plugin:import/typescript"
 );
 
+const storybookPlugin = compat.extends("plugin:storybook/recommended");
+
 const nextCoreWebVitals = compat.extends("next/core-web-vitals");
 const nextTypescript = compat.extends("next/typescript");
 
 /** @type {import('eslint').Linter.Config[]} */
 export default tseslint.config(
   js.configs.recommended,
+  // eslint-disable-next-line import/no-named-as-default-member
   ...tseslint.configs.recommended,
   ...nextCoreWebVitals,
   ...nextTypescript,
-  ...importPlugin
+  ...importPlugin,
+  ...storybookPlugin
 );
