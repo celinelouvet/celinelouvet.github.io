4b592deb79bd1bb954cb2d458760677e19d82b10
Céline Louvet <celine.louvet@shine.fr>
2025-04-13 21:48:33 +0200

    Remove Moscow

--- apps/website/src/components/slideDecks/choiceMustGoOn/contents/slides.ts
+++ apps/website/src/components/slideDecks/choiceMustGoOn/contents/slides.ts
@@ -11,14 +11,13 @@ import KnowYourExisting from './knowYourExisting';
 import KnowYourNeed from './knowYourNeed';
 import MainTitle from './mainTitle';
 import Preface from './preface';
-import ScopeYourNeed from './scopeYourNeed';
 
 export const getSlides = () => {
   return ([] as Slide[]).concat(
     MainTitle,
     Preface,
     KnowYourNeed,
-    ScopeYourNeed,
+    // ScopeYourNeed,
     DefineYourObjectives,
     KnowYourExisting,
     ForEachPossibleChoice,
