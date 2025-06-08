c8926211913f2d69c425d8454da5d1c3c27b5b4c
Céline Louvet <celine@louvet.me>
2025-06-08 19:20:06 +0200

    Fix typo

--- apps/website/src/theme/semantic-tokens.config.ts
+++ apps/website/src/theme/semantic-tokens.config.ts
@@ -10,14 +10,14 @@ const brand900_rgb = '243, 248, 251';
 const brandOrange_rgb = '207, 139, 94';
 
 const brandGradient = `180deg, rgba(${brand100_rgb}, 1), rgba(${brand100_rgb}, 1) 93%, rgba(${brand100_rgb}, 0) 93%, rgba(${brand100_rgb}, 0) 96%, rgba(${brand100_rgb}, 1) 96%`;
-const strikeBrandGradiant = `to right bottom, transparent 47%, rgba(${brand900_rgb}, 1) 47%, rgba(${brand900_rgb}, 1) 49%, rgba(${brand500_rgb}, 1) 49%, rgba(${brand500_rgb}, 1) 51%, rgba(${brand900_rgb}, 1) 51%, rgba(${brand900_rgb}, 1) 53%, transparent 52%`;
-const strikeOrangeGradiant = `to right bottom, transparent 47%, rgba(${brand900_rgb}, 1) 47%, rgba(${brand900_rgb}, 1) 49%, rgba(${brandOrange_rgb}, 1) 49%, rgba(${brandOrange_rgb}, 1) 51%, rgba(${brand900_rgb}, 1) 51%, rgba(${brand900_rgb}, 1) 53%, transparent 52%`;
+const strikeBrandGradient = `to right bottom, transparent 47%, rgba(${brand900_rgb}, 1) 47%, rgba(${brand900_rgb}, 1) 49%, rgba(${brand500_rgb}, 1) 49%, rgba(${brand500_rgb}, 1) 51%, rgba(${brand900_rgb}, 1) 51%, rgba(${brand900_rgb}, 1) 53%, transparent 52%`;
+const strikeOrangeGradient = `to right bottom, transparent 47%, rgba(${brand900_rgb}, 1) 47%, rgba(${brand900_rgb}, 1) 49%, rgba(${brandOrange_rgb}, 1) 49%, rgba(${brandOrange_rgb}, 1) 51%, rgba(${brand900_rgb}, 1) 51%, rgba(${brand900_rgb}, 1) 53%, transparent 52%`;
 
 export const semanticTokens = {
   gradients: {
     brand: { value: `linear-gradient(${brandGradient})` },
-    strike: { value: `linear-gradient(${strikeBrandGradiant})` },
-    'strike-orange': { value: `linear-gradient(${strikeOrangeGradiant})` },
+    strike: { value: `linear-gradient(${strikeBrandGradient})` },
+    'strike-orange': { value: `linear-gradient(${strikeOrangeGradient})` },
   },
   sizes: {
     'picture.size': { value: `${pictureInPx}px` },
