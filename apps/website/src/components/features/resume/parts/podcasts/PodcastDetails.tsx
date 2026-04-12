336e66a2c6d83616a1a26da932ef2b9a07ca1395
Céline Louvet <celine@louvet.me>
2026-04-12 16:33:37 +0200

    Style podcast

--- apps/website/src/components/features/resume/parts/podcasts/PodcastDetails.tsx
+++ apps/website/src/components/features/resume/parts/podcasts/PodcastDetails.tsx
@@ -1,10 +1,10 @@
-import { HStack, Stack, chakra } from '@chakra-ui/react';
+import { Box, Flex, HStack, Stack, chakra } from '@chakra-ui/react';
 import * as React from 'react';
 
 import { DateFormatter, H3Heading } from '@/components/core';
 import { type Podcast } from '@/data';
 
-import { AuthorText, DescriptionText, EpisodeText, TitleText } from './parts';
+import { AuthorText, DescriptionText, TitleText } from './parts';
 
 export type PodcastDetailsProps = {
   podcast: Podcast;
@@ -15,21 +15,35 @@ export const PodcastDetails: React.FC<PodcastDetailsProps> =
     const { name, author, title, language, when, episode, description } =
       podcast;
     return (
-      <Stack gap="2">
-        <Stack
-          alignItems="baseline"
-          gap="2"
-          flexWrap="wrap"
-          flexDirection={{ base: 'row', md: 'row' }}
-        >
-          <H3Heading>{name}</H3Heading>
-          <EpisodeText episode={episode} />
-          <DateLanguage language={language} when={when} />
-        </Stack>
-        <TitleText title={title} />
-        <AuthorText author={author} />
-        <DescriptionText description={description} />
-      </Stack>
+      <>
+        <Box display={{ base: 'block', md: 'none', _print: 'block' }}>
+          <Stack gap="2">
+            <Stack gap="0">
+              <Flex alignItems="baseline" gap="2" flexWrap="wrap">
+                <H3Heading>{name}</H3Heading>
+                <DateLanguage language={language} when={when} />
+              </Flex>
+              <TitleText title={title} episode={episode} />
+            </Stack>
+            <AuthorText author={author} />
+            <DescriptionText description={description} />
+          </Stack>
+        </Box>
+
+        <Box display={{ base: 'none', md: 'block', _print: 'none' }}>
+          <Stack gap="2">
+            <Stack gap="0">
+              <Flex alignItems="baseline" gap="2" flexWrap="wrap">
+                <H3Heading>{name}</H3Heading>
+                <DateLanguage language={language} when={when} />
+              </Flex>
+              <TitleText title={title} episode={episode} />
+            </Stack>
+            <AuthorText author={author} />
+            <DescriptionText description={description} />
+          </Stack>
+        </Box>
+      </>
     );
   };
 
